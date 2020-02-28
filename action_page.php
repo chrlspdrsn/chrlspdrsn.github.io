08147c320b3d73198f67150b8c9b7c911a26535a
chrlspdrsn <61101059+chrlspdrsn@users.noreply.github.com>
2020-02-27 20:29:13 -0800

    updates for contact page
    added php and additional information

--- action_page.php
+++ action_page.php
@@ -1,3 +1,18 @@
 <?php
+  $name = $_POST['name'];
+  $userEmail = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
->
+  $email_from = 'pedersoncharlie@gmail.com';
+  $email_subject = "New Form Submission";
+  $email_body = "User Name: $name.\n".
+        "User Email: $userEmail.\n".
+        "Subject: $subject.\n".
+        "Message: $message.\n";
+  $to = "pedersoncharlie@gmail.com";
+
+  mail($to,$email_subject,$email_body);
+  header("Location: contact.html");
+
+?>
