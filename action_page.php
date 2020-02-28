c4a9276a987a9c28e8317f31182b82a5d2989b81
chrlspdrsn <61101059+chrlspdrsn@users.noreply.github.com>
2020-02-27 20:36:52 -0800

    test 2 php

--- action_page.php
+++ action_page.php
@@ -4,15 +4,21 @@
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-  $email_from = 'pedersoncharlie@gmail.com';
+  $email_from = "pedersoncharlie@gmail.com";
   $email_subject = "New Form Submission";
   $email_body = "User Name: $name.\n".
         "User Email: $userEmail.\n".
         "Subject: $subject.\n".
         "Message: $message.\n";
+
   $to = "pedersoncharlie@gmail.com";
 
-  mail($to,$email_subject,$email_body);
+  $headers = "From: $email_from \r\n";
+
+  $headers = "Reply-To: $userEmail\r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+  
   header("Location: contact.html");
 
 ?>
